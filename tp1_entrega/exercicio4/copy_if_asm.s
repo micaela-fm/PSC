6301c4e528b8ae7d17c4d051040452477ba8433c
micaelafm <91810255+micaela-fm@users.noreply.github.com>
2025-11-16 23:16:12 +0000

    fix(ex4): save registers (not yet working)

--- tp1_entrega/exercicio4/copy_if_asm.s
+++ tp1_entrega/exercicio4/copy_if_asm.s
@@ -14,35 +14,51 @@ size_t copy_if(void *dst, void *src, size_t src_size, size_t elem_size,
 	.text
 	.global	copy_if
 copy_if:
-	mov		%rdi, %r10			# *iter = dst
-	mov		%rdi, %r11			# *dst_iter = dst
-	push	%r12				
-	xor		%r12, %r12
-	imul	%rcx, %rdx, %r12	# last = src_size * elem_size
+	mov		%rdi, %r11			# dst_iter = dst
+	push	%rdi
+	push	%r12	
+	mov		%rdx, %r12		
+	imul	%rcx, %r12			# last = src_size * elem_size
 	add		%rsi, %r12			# last += src
 	mov		%rsi, %r10			# iter = src
 for:
 	cmp		%r12, %r10			# iter < last
 	jge		for_end
 if:
-	push	%rdi
+	push	%r10
+	push	%r11	
+	push	%rcx
+	push	%rsi
 	mov		%r10, %rdi			# 1st arg: iter
 	mov		%r9, %rsi			# 2nd arg: context
-	call 	(%r8)				# predicate(iter, context)
-	pop		%rdi
-	cmp		$0, %rax
+	call 	*%r8				# predicate(iter, context)
+	pop		%rsi
+	pop		%rcx
+	pop		%r11
+	pop		%r10
+	test	%rax, %rax
 	jz		if_end
-	# TODO: memcpy(dst_iter, iter, elem_size)
+	push	%rcx
+	push	%r10
+	push	%r11
+	mov		%r11, %rdi			# 1st arg: dst_iter
+	mov		%r10, %rsi			# 2nd arg: iter
+	mov		%rcx, %rdx			# 3rd arg: elem_size
+	call	memcpy				# memcpy(dst_iter, iter, elem_size);
+	pop		%r11
+	pop		%r10
+	pop		%rcx
 	add		%rcx, %r11			# dst_iter += elem_size;
 if_end:
 	add		%rcx, %r10			# iter += elem_size
 	jmp 	for
 for_end:
 	pop		%r12
-	mov		%rdi, %rax			# rax = dst
-	div		%rcx				# dst /= elem_size
-	sub		%rax, %r11			# dst_iter -= dst
-	mov		%r11, %rax			# return r11
+	pop		%rdi
+	mov		%r11, %rax
+	sub		%rdi, %rax
+	xor		%rdx, %rdx
+	div		%rcx
 	ret
 
 	.section	.note.GNU-stack
