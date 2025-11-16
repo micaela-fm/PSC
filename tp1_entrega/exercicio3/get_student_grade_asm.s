c9fee390213aa5bb1a8c9a05447b4faebf1d4c5c
micaelafm <91810255+micaela-fm@users.noreply.github.com>
2025-11-16 18:59:42 +0000

    fix(ex3): ex3 now working

--- tp1_entrega/exercicio3/get_student_grade_asm.s
+++ tp1_entrega/exercicio3/get_student_grade_asm.s
@@ -28,28 +28,28 @@ short get_student_grade(Class *class, int number, int grade_idx) {
 	.text
 	.global	get_student_grade
 get_student_grade:
-	mov		$-1, %rax							# short grade = -1
+	mov		$-1, %rax								# short grade = -1
 if_grade_idx:
-	cmp		$4, %edx							# if (grade_idx < 4)
+	cmp		$4, %edx								# if (grade_idx < 4)
 	jge		if_grade_idx_end
-	mov 	$0, %ecx							# int i = 0
+	mov 	$0, %ecx								# int i = 0
 for:
-	cmpl	length(%rdi), %ecx					# i < class->length
+	cmpl	length(%rdi), %ecx						# i < class->length
 	jge		for_end
 if_number:
 	mov		%ecx, %r9d
-	imul	student_size, %r9d					# i *= student_size, as it is not a valid scale factor
-	movslq	%r9d, %r9							# index register must be quad-word
-	mov		students(%rdi), %r8					# r8 = class->students pointer
-	add		%r9, %r8							# r8 = students[i]
-	cmpl	number(%r8), %esi					# if class->students[i].number == number
+	imul	$student_size, %r9d						# i *= student_size, as it is not a valid scale factor
+	movslq	%r9d, %r9								# index register must be quad-word
+	movq	students(%rdi), %r8						# r8 = class->students pointer
+	add		%r9, %r8								# r8 = students[i]
+	cmpl	number(%r8), %esi						# if class->students[i].number == number
 	jne		if_number_end
-	movslq	%edx, %rdx							# index register must be quad-word
+	movslq	%edx, %rdx								# index register must be quad-word
 	movswl	grades(%r8, %rdx, grades_size), %eax	# grade = class‐>students[i].grades[grade_idx]
 	movslq	%eax, %rax
-	jmp		for_end								# break
+	jmp		for_end									# break
 if_number_end:
-	inc		%ecx								# i++
+	inc		%ecx									# i++
 	jmp		for
 for_end:
 if_grade_idx_end:
